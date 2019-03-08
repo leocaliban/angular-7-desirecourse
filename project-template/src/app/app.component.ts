c33df82dacc29fa9313143ba9a80cf40f21900f7
Leocaliban <leocaliban@gmail.com>
2019-03-08 19:59:35 -0300

    Property Binding

--- project-template/src/app/app.component.ts
+++ project-template/src/app/app.component.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 }
