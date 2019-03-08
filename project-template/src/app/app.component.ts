d40d3aa1d44ba7df313344a542cd633af20517c6
Leocaliban <leocaliban@gmail.com>
2019-03-08 20:11:13 -0300

    Event Binding

--- project-template/src/app/app.component.ts
+++ project-template/src/app/app.component.ts
@@ -8,10 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
+  }
+
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
   }
 }
