af011dc7479104951ffa855143e10afcb9dcb929
Leocaliban <leocaliban@gmail.com>
2019-03-08 19:50:15 -0300

    Interpolação

--- project-template/src/app/server/server.component.ts
+++ project-template/src/app/server/server.component.ts
@@ -9,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
+  serverId = 10;
+  serverStatus = 'offline';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getServerStatus(): string {
+    return this.serverStatus;
   }
 
 }
