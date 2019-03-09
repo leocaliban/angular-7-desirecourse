7ab538c677dd04824dce2430e0dcaeb8dfdefc67
Leocaliban <leocaliban@gmail.com>
2019-03-09 12:45:15 -0300

    Diretiva ngStyle

--- project-template/src/app/server/server.component.ts
+++ project-template/src/app/server/server.component.ts
@@ -12,13 +12,19 @@ export class ServerComponent implements OnInit {
   serverId = 10;
   serverStatus = 'offline';
 
-  constructor() { }
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   ngOnInit() {
   }
 
   getServerStatus(): string {
     return this.serverStatus;
+  }
+
+  getColor(): string {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 
 }
