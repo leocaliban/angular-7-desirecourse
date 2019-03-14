9bccf714492acd54ef23b9c5f2988b2be3f8f9a9
Leocaliban <leocaliban@gmail.com>
2019-03-14 02:32:20 -0300

    Cria diretiva de atributo básica

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
