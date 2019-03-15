f37be6161f36f415f50b80e302148acad5a95371
Leocaliban <leocaliban@gmail.com>
2019-03-15 01:22:53 -0300

    Diretiva Usa Renderer2 para manipular elements DOM

---
+++ directives-start/src/app/better-highlight.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+
+  constructor(
+    private renderer: Renderer2,
+    private elementRef: ElementRef) { }
+
+  ngOnInit() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+  }
+
+}
