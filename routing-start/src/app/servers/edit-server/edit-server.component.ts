5728c669a852c10c4c774ab4e60978bfd4fe94e8
Leocaliban <leocaliban@gmail.com>
2019-03-18 01:27:37 -0300

    Cria nova rota para servers

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -28,5 +28,4 @@ export class EditServerComponent implements OnInit {
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, { name: this.serverName, status: this.serverStatus });
   }
-
 }
