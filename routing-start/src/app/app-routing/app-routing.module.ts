e3c7e01ad463f9c0a4503545b8803ec61a36ca51
Leocaliban <leocaliban@gmail.com>
2019-03-18 19:14:20 -0300

    Passing Static Data to a Route

--- routing-start/src/app/app-routing/app-routing.module.ts
+++ routing-start/src/app/app-routing/app-routing.module.ts
@@ -10,6 +10,7 @@ import { EditServerComponent } from '../servers/edit-server/edit-server.componen
 import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
 import { AuthGuardGuard } from '../auth-guard.guard';
 import { CanDeactivateGuard } from '../servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from '../error-page/error-page.component';
 
 const appRoutes: Routes = [
   {
@@ -45,7 +46,10 @@ const appRoutes: Routes = [
   },
   {
     path: 'not-found',
-    component: PageNotFoundComponent
+    component: ErrorPageComponent,
+    data:{
+      message: 'Page not found!'
+    }
   },
   {
     path: '**',
--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.s
     UserComponent,
     EditServerComponent,
     ServerComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
