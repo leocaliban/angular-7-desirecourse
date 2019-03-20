437d1771b3a5338acabdd44416912d04cd2c286d
Leocaliban <leocaliban@gmail.com>
2019-03-20 01:27:28 -0300

    Add navigation to the app

--- master-food/src/app/header/header.component.ts
+++ master-food/src/app/header/header.component.ts
@@ -1,21 +1,10 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
-  @Output() featureSelected = new EventEmitter<string>();
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
+export class HeaderComponent {
 
 }
