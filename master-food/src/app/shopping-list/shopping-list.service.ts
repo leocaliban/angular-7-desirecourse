d4e02398d92e3d87fcdb942cd3b9471f31e37d47
Leocaliban <leocaliban@gmail.com>
2019-04-02 10:09:05 -0300

    Aplica Reactive service com Observables (Subjects)

--- master-food/src/app/shopping-list/shopping-list.service.ts
+++ master-food/src/app/shopping-list/shopping-list.service.ts
@@ -1,12 +1,13 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
 
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
   private ingredients: Ingredient[] = [];
 
   constructor() { }
@@ -26,6 +27,6 @@ export class ShoppingListService {
   }
 
   emmitChange() {
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
