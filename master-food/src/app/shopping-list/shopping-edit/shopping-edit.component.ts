ea75606e565e90475c354aac75531edcf0e74e65
Leocaliban <leocaliban@gmail.com>
2019-04-04 20:29:24 -0300

    Updating existing Items

--- master-food/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ master-food/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -31,14 +31,18 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
         this.shoppingListForm.setValue({
           name: this.editedItem.name,
           amount: this.editedItem.amount
-        })
+        });
       });
   }
 
   onAddItem(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.shoppingListService.addIngredient(newIngredient);
+    if (this.editMode) {
+      this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.shoppingListService.addIngredient(newIngredient);
+    }
   }
 
   ngOnDestroy(): void {
