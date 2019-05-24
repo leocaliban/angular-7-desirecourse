07ef5e8fd9c8121d2ad40d7735df0563d6736efb
Leocaliban <leocaliban@gmail.com>
2019-05-24 12:30:35 -0300

    Transforming Response Data to Prevent Errors

--- master-food/src/app/shared/data-storage.service.ts
+++ master-food/src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -21,8 +22,17 @@ export class DataStorageService {
 
   getRecipes() {
     return this.http.get(`${this.URL}recipes.json`)
-      .subscribe((response: Response) => {
-        const recipes: Recipe[] = response.json();
+      .pipe(
+        map((response: Response) => {
+          const recipes: Recipe[] = response.json();
+          for (const recipe of recipes) {
+            if (!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }))
+      .subscribe((recipes: Recipe[]) => {
         this.recipeService.setRecipes(recipes);
       });
   }
