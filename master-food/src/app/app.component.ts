4e3ca2d1eb83510689cb617cdb93c4fff9a4947e
Leocaliban <leocaliban@gmail.com>
2019-05-25 11:49:21 -0300

    Criação de usuários integrado ao firebase

--- master-food/src/app/app.component.ts
+++ master-food/src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   loadedFeature = 'recipe';
+
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCW4JYP_2bKjnERM2W7JxodbJuZqUQ3TpY',
+      authDomain: 'ng-master-food.firebaseapp.com',
+      databaseURL: 'https://ng-master-food.firebaseio.com',
+      projectId: 'ng-master-food',
+      storageBucket: 'ng-master-food.appspot.com'
+    });
+  }
+
   onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
