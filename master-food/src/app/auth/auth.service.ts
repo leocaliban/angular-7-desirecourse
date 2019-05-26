147205e9d621686e194451f160ae504d411dbf59
Leocaliban <leocaliban@gmail.com>
2019-05-26 18:13:35 -0300

    Aplica redirecionamento e proteção de rotas com Guards

--- master-food/src/app/auth/auth.service.ts
+++ master-food/src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,9 @@ export class AuthService {
 
   token: string;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -20,6 +23,7 @@ export class AuthService {
   signinUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(response => {
+        this.router.navigate(['/']);
         firebase.auth().currentUser.getIdToken().then((token: string) => {
           this.token = token;
         });
