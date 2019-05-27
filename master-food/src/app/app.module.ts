32d27b0b28824b891984d104ebfd784e015be299
Leocaliban <leocaliban@gmail.com>
2019-05-27 11:56:39 -0300

    Creating a Shopping List Feature Module

--- master-food/src/app/app.module.ts
+++ master-food/src/app/app.module.ts
@@ -1,34 +1,31 @@
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     SignupComponent,
     SigninComponent
   ],
   imports: [
     HttpModule,
-    FormsModule,
     SharedModule,
     RecipesModule,
     BrowserModule,
     AppRoutingModule,
+    ShoppingListModule
   ],
   bootstrap: [AppComponent]
 })
