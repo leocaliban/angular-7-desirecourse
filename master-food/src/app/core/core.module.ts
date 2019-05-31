9f9513ddda530b52cdc47b616c8bf54935f8b12d
Leocaliban <leocaliban@gmail.com>
2019-05-31 17:38:04 -0300

    Cria interceptor

--- master-food/src/app/core/core.module.ts
+++ master-food/src/app/core/core.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
 import { HeaderComponent } from './header/header.component';
+import { AuthInterceptor } from '../shared/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,13 @@ import { HeaderComponent } from './header/header.component';
   exports: [
     HeaderComponent,
     AppRoutingModule,
+  ],
+  providers:[
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ]
 })
 export class CoreModule { }
