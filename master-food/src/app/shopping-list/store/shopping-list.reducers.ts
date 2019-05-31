91248751626deeb6254dff49a46c0695e9b179e9
Leocaliban <leocaliban@gmail.com>
2019-05-31 19:06:13 -0300

    Cria e registra um reducer

---
+++ master-food/src/app/shopping-list/store/shopping-list.reducers.ts
@@ -0,0 +1,21 @@
+import * as ShoppingListActions from './shopping-list.actions';
+import { Ingredient } from '../../shared/ingredient.model';
+
+const initialState = {
+  ingredients: [
+    new Ingredient('Apple', 5),
+    new Ingredient('Banana', 10),
+  ]
+};
+export function shoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions) {
+  switch (action.type) {
+    case ShoppingListActions.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload]
+      };
+
+    default:
+      return state;
+  }
+}
