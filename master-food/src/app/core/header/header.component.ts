1567b3f098743ed2e104f6a4d9bfac53ca27c7ee
Leocaliban <leocaliban@gmail.com>
2019-06-02 19:58:37 -0300

    Using Authentication

--- master-food/src/app/core/header/header.component.ts
+++ master-food/src/app/core/header/header.component.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Response } from '@angular/http';
 
 import { AuthService } from '../../auth/auth.service';
 import { DataStorageService } from '../../shared/data-storage.service';
+import * as fromApp from '../../ngrx-store/app.reducers';
+import * as fromAuth from '../../auth/store/auth.reducers';
+
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  authState: Observable<fromAuth.State>;
 
   constructor(
     public authService: AuthService,
     private dataStorageService: DataStorageService,
+    private store: Store<fromApp.AppState>
   ) { }
+
+  ngOnInit(): void {
+    this.authState = this.store.select('auth');
+  }
 
   onSaveData() {
     this.dataStorageService.storeRecipes().subscribe(
--- master-food/src/app/ngrx-store/app.reducers.ts
+++ master-food/src/app/ngrx-store/app.reducers.ts
@@ -4,8 +4,8 @@ import * as fromShoppingList from '../shopping-list/store/shopping-list.reducers
 import { ActionReducerMap } from '@ngrx/store';
 
 export interface AppState {
-  shoppingList: fromShoppingList.State,
-  auth: fromAuth.State,
+  shoppingList: fromShoppingList.State;
+  auth: fromAuth.State;
 }
 
 export const REDUCERS: ActionReducerMap<AppState> = {
