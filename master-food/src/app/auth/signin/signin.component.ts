512711d7ff6374589565807ce88256eaef846ad1
Leocaliban <leocaliban@gmail.com>
2019-06-03 20:13:00 -0300

    Adding Auth Signin

--- master-food/src/app/auth/signin/signin.component.ts
+++ master-food/src/app/auth/signin/signin.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth.service';
 import { NgForm } from '@angular/forms';
+import { Store } from '@ngrx/store';
+
+import * as fromApp from '../../ngrx-store/app.reducers';
+import * as AuthActions from '../store/auth.actions';
 
 @Component({
   selector: 'app-signin',
@@ -9,9 +12,7 @@ import { NgForm } from '@angular/forms';
 })
 export class SigninComponent implements OnInit {
 
-  constructor(
-    private authService: AuthService
-  ) { }
+  constructor(private store: Store<fromApp.AppState>) { }
 
   ngOnInit() {
   }
@@ -19,7 +20,10 @@ export class SigninComponent implements OnInit {
   onSignin(form: NgForm) {
     const email = form.value.email;
     const password = form.value.password;
-    this.authService.signinUser(email, password);
+    this.store.dispatch(new AuthActions.TrySignin({
+      username: email,
+      password
+    }));
 
   }
 }
--- master-food/src/app/auth/store/auth.actions.ts
+++ master-food/src/app/auth/store/auth.actions.ts
@@ -2,9 +2,16 @@ import { Action } from '@ngrx/store';
 
 export const TRY_SIGNUP = 'TRY_SIGNUP';
 export const SIGNUP = 'SIGNUP';
+export const TRY_SIGNIN = 'TRY_SIGNIN';
 export const SIGNIN = 'SIGNIN';
 export const LOGOUT = 'LOGOUT';
 export const SET_TOKEN = 'SET_TOKEN';
+
+export class TrySignin implements Action {
+  readonly type: string = TRY_SIGNIN;
+
+  constructor(public payload: { username: string, password: string }) { }
+}
 
 export class TrySignup implements Action {
   readonly type: string = TRY_SIGNUP;
@@ -35,4 +42,4 @@ export class SetToken implements Action {
   constructor(public payload: string) { }
 }
 
-export type AuthActions = Signup | Signin | Logout | SetToken | TrySignup;
+export type AuthActions = Signup | Signin | Logout | SetToken | TrySignup | TrySignin;
