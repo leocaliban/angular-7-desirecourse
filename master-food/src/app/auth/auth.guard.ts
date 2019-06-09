3141bd1a03ba7937869f2f9f050ea2ff2fd8f90a
Leocaliban <leocaliban@gmail.com>
2019-06-09 03:57:31 -0300

    FIX AuthGuard

--- master-food/src/app/auth/auth.guard.ts
+++ master-food/src/app/auth/auth.guard.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 
-import { Observable } from 'rxjs';
 import * as fromApp from '../ngrx-store/app.reducers';
 import * as fromAuth from './store/auth.reducers';
+
+import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,8 +19,10 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.store.select('auth')
-      .pipe(map((authState: fromAuth.State) => {
-        return authState.authenticated;
-      }));
+      .pipe(
+        take(1),
+        map((authState: fromAuth.State) => {
+          return authState.authenticated;
+        }));
   }
 }
