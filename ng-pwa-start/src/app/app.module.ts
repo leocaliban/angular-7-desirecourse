5bd6cf00fa4014d16912b24ce1df58a5743fcb86
Leocaliban <leocaliban@gmail.com>
2019-06-12 06:22:47 -0300

    Adding Service Workers

--- ng-pwa-start/src/app/app.module.ts
+++ ng-pwa-start/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [AppComponent, PostComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js',
+      { enabled: environment.production })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
